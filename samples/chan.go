5ae6a4fc6985379cb6ce5ebd58f7254607fe4016
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2022-11-22 18:02:09 +0530

    make go chan sample wait for all goroutines

--- samples/chan.go
+++ samples/chan.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
 
 func sendvals(c chan int, count int) {
+	wg.Add(1)
 	for i := 0; i < count; i++ {
 		c <- i
 	}
@@ -12,12 +18,14 @@ func recvvals(c chan int, count int) {
 	for i := 0; i < count; i++ {
 		fmt.Println(<-c)
 	}
+	wg.Done()
 }
 
 func main() {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100; i++ {
 		c := make(chan int)
 		go sendvals(c, 100)
 		go recvvals(c, 100)
 	}
+	wg.Wait()
 }
