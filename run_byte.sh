13598ace247f46ff353623a0dfcad6d52df3956a
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-02-04 11:45:59 +0530

    add run_byte.sh

---
+++ run_byte.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#  check if args are passed
+if [ $# -eq 0 ]; then
+    echo "usage: $0 <file>"
+    exit 1
+fi
+
+#  check if file exists
+if [ ! -f $1 ]; then
+    echo "error: file $1 does not exist"
+    exit 1
+fi
+
+./target/debug/sahl $1 -c
+./sahl exe.bin
