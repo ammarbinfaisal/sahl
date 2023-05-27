19b005494965c400450cebda8ecdeb7f6ed72233
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-05-27 19:26:57 +0530

    update testing script

--- test.py
+++ test.py
@@ -42,7 +42,7 @@ def run_file(file, only_rc_check=False):
     rc = p.returncode
     if rc != 0:
         print(f"{file} failed")
-        print(err.decode("utf-8"))
+        print(err.decode("utf-8") if len(err) > 0 else output.decode("utf-8"), end="")
         return
     if only_rc_check:
         passed(file)
@@ -65,6 +65,8 @@ if __name__ == "__main__":
         run_file(file, only_rc_check=True)
     result = f"{passing}/{total}"
     print(f"{result} tests passed")
+    if not "GITHUB_ENV" in environ:
+        exit(0)
     with open(environ["GITHUB_ENV"], "w") as f:
         f.write(f"TEST_RESULT={result}\n")
         f.write(f"COLOR={int((passing / total) * 100)}\n")
