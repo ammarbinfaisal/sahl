b02d879f21df876ccaba26c112d3350b4454ec33
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-05-29 12:40:30 +0530

    add run_go.sh

---
+++ run_go.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#  check if args are passed
+if [ $# -eq 0 ]; then
+    echo "usage: $0 <file>"
+    exit 1
+fi
+
+#  check if file exists
+if [ ! -f $1 ]; then
+    echo "error: file $1 does not exist"
+    exit 1
+fi
+
+./target/release/sahl $1 -g > prog.go && go run prog.go
