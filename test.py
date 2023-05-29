f629de2633f97bc8b9bddf769ccfea0e97b2a4d8
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-05-29 15:12:55 +0530

    update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 from os import environ
+from sys import argv
 
 files_all_checks = [
     "addition",
@@ -40,6 +41,11 @@ def passed(file):
     passing += 1
     print(f"{file} passed")
 
+def passed_go(file):
+    global passing_go
+    passing_go += 1
+    print(f"{file} passed using go")
+
 def run_file_byte(file, only_rc_check=False):
     print(f"running {file} in byte code")
     p = Popen(["./run_byte.sh", f"samples/{file}.sahl"], stdout=PIPE, stderr=PIPE)
@@ -77,7 +83,7 @@ def run_file_aot(file, only_rc_check=False):
     global passing_aot
     passing_aot += 1
 
-def run_file_go(file):
+def run_file_go(file, only_rc_check=False):
     print(f"running {file} in go")
     p = Popen(["./run_go.sh", f"samples/{file}.sahl"], stdout=PIPE, stderr=PIPE)
     output, err = p.communicate(b"")
@@ -86,26 +92,68 @@ def run_file_go(file):
         print(f"{file} failed in go")
         print(err.decode("utf-8") if len(err) > 0 else output.decode("utf-8"), end="")
         return
-    global passing_go
-    passing_go += 1
+    if only_rc_check:
+        passed_go(file)
+        return
+    output = output.decode("utf-8")
+    with open(f"tests/{file}", "r") as f:
+        expected = f.read()
+    if expected == output:
+        passed_go(file)
+    else:
+        print(f"{file} failed in go")
 
-if __name__ == "__main__":
+def run_all_go():
+    for file in files_all_checks:
+        run_file_go(file)
+
+def run_all_byte():
     for file in files_all_checks:
         run_file_byte(file)
+
+def run_all_aot():
+    for file in files_all_checks:
         run_file_aot(file)
-        run_file_go(file)
-    for file in files_retcode_check:
-        run_file_byte(file, only_rc_check=True)
-        run_file_aot(file, only_rc_check=True)
-        run_file_go(file)
-    result = f"{passing}/{total}"
-    print(f"{result} bytecode tests passed")
-    print(f"{passing_aot}/{total} aot tests passed")
+
+def go_result():
     print(f"{passing_go}/{total} go tests passed")
+
+def aot_result():
+    print(f"{passing_aot}/{total} aot tests passed")
+
+def byte_result():
+    print(f"{passing}/{total} bytecode tests passed")
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        if argv[1] == "go":
+            run_all_go()
+            go_result()
+        elif argv[1] == "aot":
+            run_all_aot()
+            aot_result()
+        elif argv[1] == "byte":
+            run_all_byte()
+            byte_result()
+        elif argv[1] in files_all_checks:
+            run_file_byte(argv[1])
+            run_file_aot(argv[1])
+            run_file_go(argv[1])
+        elif argv[1] in files_retcode_check:
+            run_file_byte(argv[1], only_rc_check=True)
+            run_file_aot(argv[1], only_rc_check=True)
+            run_file_go(argv[1], only_rc_check=True)
+        exit(0)
+    run_all_byte()
+    run_all_aot()
+    run_all_go()
+    byte_result()
+    aot_result()
+    go_result()
     if not "GITHUB_ENV" in environ:
         exit(0)
     with open(environ["GITHUB_ENV"], "w") as f:
-        f.write(f"TEST_RESULT={result}\n")
+        f.write(f"TEST_RESULT={passing}/{total}\n")
         f.write(f"COLOR={int((passing / total) * 100)}\n")
         f.write(f"AOT_TEST_RESULT={passing_aot}/{total}\n")
         f.write(f"AOT_COLOR={int((passing_aot / total) * 100)}\n")
