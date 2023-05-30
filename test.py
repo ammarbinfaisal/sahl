39506bb4d4ce7ac14b5969daf0a0c505c1dafe76
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-05-30 16:33:33 +0530

    update test.py

--- test.py
+++ test.py
@@ -31,13 +31,22 @@ files_retcode_check = [
     "coroutine",
 ]
 
+byte_mode = 0
+aot_mode = 1
+go_mode = 2
+
 passing = {
-    "byte": 0,
-    "aot": 0,
-    "go": 0,
+    byte_mode: 0,
+    aot_mode: 0,
+    go_mode: 0,
 }
 total = len(files_all_checks) + len(files_retcode_check)
 
+mode_to_str = {
+    byte_mode: "byte",
+    aot_mode: "aot",
+    go_mode: "go",
+}
 
 def passed(file, mode):
     global passing
@@ -45,22 +54,23 @@ def passed(file, mode):
     print(f"{file} passed")
 
 def failed(file, mode):
-    print(f"{file} failed in {mode}")
+    print(f"{file} failed in {mode_to_str[mode]}")
 
 def run_file(mode, file, only_rc_check=False):
-    print(f"running {file} in {mode}")
-    p = Popen([f"./run_{mode}.sh", f"samples/{file}.sahl"], stdout=PIPE, stderr=PIPE)
+    modestr = mode_to_str[mode]
+    print(f"running {file} in {modestr}")
+    p = Popen([f"./run_{modestr}.sh", f"samples/{file}.sahl"], stdout=PIPE, stderr=PIPE)
     output, err = p.communicate(b"")
     rc = p.returncode
     if rc != 0:
         failed(file, mode)
         return
-    if only_rc_check or mode == "aot":
+    if only_rc_check or mode == aot_mode:
         passed(file, mode)
         return
     output = (
         output.decode("utf-8").split("\n\n\n\n\n")[1]
-        if mode == "byte"
+        if mode == byte_mode
         else output.decode("utf-8")
     )
     with open(f"tests/{file}", "r") as f:
@@ -79,7 +89,7 @@ def run_all(mode):
 def result(mode):
     print(f"{passing[mode]}/{total} {mode} tests passed")
 
-modes = ["byte", "aot", "go"]
+modes = [byte_mode, aot_mode, go_mode]
 
 if __name__ == "__main__":
     if len(argv) > 1:
@@ -87,24 +97,24 @@ if __name__ == "__main__":
             run_all(argv[1])
             result(argv[1])
         elif argv[1] in files_all_checks:
-            run_file("byte", argv[1])
-            run_file("aot", argv[1])
-            run_file("go", argv[1])
+            run_file(byte_mode, argv[1])
+            run_file(aot_mode, argv[1])
+            run_file(go_mode, argv[1])
         elif argv[1] in files_retcode_check:
-            run_file("byte", argv[1], True)
-            run_file("aot", argv[1], True)
-            run_file("go", argv[1], True)
+            run_file(byte_mode, argv[1], True)
+            run_file(aot_mode, argv[1], True)
+            run_file(go_mode, argv[1], True)
         exit(0)
-    for mode in ["byte", "aot", "go"]:
+    for mode in [byte_mode, aot_mode, go_mode]:
         run_all(mode)
-    for mode in ["byte", "aot", "go"]:
+    for mode in [byte_mode, aot_mode, go_mode]:
         result(mode)
     if not "GITHUB_ENV" in environ:
         exit(0)
     with open(environ["GITHUB_ENV"], "w") as f:
-        f.write(f"TEST_RESULT={passing['byte']}/{total}\n")
-        f.write(f"COLOR={int((passing['byte'] / total) * 100)}\n")
-        f.write(f"AOT_TEST_RESULT={passing['aot']}/{total}\n")
-        f.write(f"AOT_COLOR={int((passing['aot'] / total) * 100)}\n")
-        f.write(f"GO_TEST_RESULT={passing['go']}/{total}\n")
-        f.write(f"GO_COLOR={int((passing['go'] / total) * 100)}\n")
+        f.write(f"TEST_RESULT={passing[byte_mode]}/{total}\n")
+        f.write(f"COLOR={int((passing[byte_mode] / total) * 100)}\n")
+        f.write(f"AOT_TEST_RESULT={passing[aot_mode]}/{total}\n")
+        f.write(f"AOT_COLOR={int((passing[aot_mode] / total) * 100)}\n")
+        f.write(f"GO_TEST_RESULT={passing[go_mode]}/{total}\n")
+        f.write(f"GO_COLOR={int((passing[go_mode] / total) * 100)}\n")
