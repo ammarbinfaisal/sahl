d6f25240e0911c82a0a9d63e7dedfdf60fef44d5
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-08-22 12:00:01 +0530

    inline reads

--- vm/read.h
+++ vm/read.h
@@ -4,9 +4,29 @@
 
 #include <stdint.h>
 
-uint32_t read_u32(uint8_t *code, int idx);
-uint64_t read_u64(uint8_t *code, int idx);
-double read_double(uint8_t *code, int idx);
+inline uint32_t read_u32(uint8_t *code, int idx) {
+    union {
+        uint8_t *restrict u8;
+        uint32_t *restrict u32;
+    } conv = {code + idx};
+    return *conv.u32;
+}
+
+inline uint64_t read_u64(uint8_t *code, int idx) {
+    union {
+        uint8_t *restrict u8;
+        uint64_t *restrict u64;
+    } conv = {code + idx};
+    return *conv.u64;
+}
+
+inline double read_double(uint8_t *code, int idx) {
+    union {
+        uint8_t *restrict u8;
+        double *restrict d;
+    } conv = {code + idx};
+    return *conv.d;
+}
 char *read_string(uint8_t *code, int idx, int len);
 
 #endif
