23929a48eb1fef7fcf4238627e07721c8458a93c
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-09-01 14:45:23 +0530

    delete sahl_rts.c

--- sahl_rts.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void print_str(char *str) { puts(str); }
-
-void print_int(long long i) { printf("%lld\n", i); }
-
-void print_char(char c) { printf("%c\n", c); }
-
-void print_bool(int b) { printf("%s\n", b ? "true" : "false"); }
