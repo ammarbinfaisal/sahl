faec0d3afe63cd977c3190fab245be386ecd791e
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-11-14 18:01:32 +0530

    Optimize code by adding -O3 flag to clang compiler

--- run_aot.sh
+++ run_aot.sh
@@ -12,4 +12,4 @@ if [ ! -f $1 ]; then
     exit 1
 fi
 
-./target/release/sahl $1 -n 2>./exe.ll && clang -g ./exe.ll ./rt.c -o ./exe && ./exe
+./target/release/sahl $1 -n 2>./exe.ll && clang -g -O3 ./exe.ll ./rt.c -o ./exe && ./exe
