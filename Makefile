19c29ae9d622503641f47cb69b77f5c19f6c3ad8
Malik Ammar Faisal <binfaisal.ammar@gmail.com>
2023-11-21 14:51:41 +0530

    Add garbage collection support

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: vm
 
-all: codegen vm rts
+all: codegen vm rts gc
 
 codegen:
 	cd frontend && cargo build --release
@@ -10,6 +10,9 @@ rts:
 
 vm: vm/*
 	gcc vm/* -o sahl -O2 -g -lm -lpthread
+
+gc:
+	cd bdwgc && ./autogen.sh && ./configure --enable-threads=posix --enable-large-config --enable-parallel-mark --enable-static --disable-shared --disable-gcc-warnings && make 
 
 check:
 	python3 test.py
--- run_aot.sh
+++ run_aot.sh
@@ -12,7 +12,22 @@ if [ ! -f $1 ]; then
     exit 1
 fi
 
+function check {
+    if [ $? -ne 0 ]; then
+        echo "error: failed to compile $1"
+        exit 1
+    fi
+}
+
 # $rt=./runtime/target/release/libruntime.so
 rt=./rt.c
 
-./frontend/target/release/sahl $1 -n 2>./exe.ll && clang -opaque-pointer -lgc -lm -O3 ./exe.ll $rt -o ./exe && ./exe
+./frontend/target/release/sahl $1 -n 2>./exe.ll
+
+check $1
+
+clang -opaque-pointer -I ./bdwgc/include -O2 -lgc ./exe.ll ./bdwgc/.libs/libgc.a $rt  -o ./exe
+
+check $1
+
+./exe
